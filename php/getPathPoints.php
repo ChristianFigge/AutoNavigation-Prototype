fedb3a48602fc6789634307ca6d9b010884650bd
ChristianFigge <christian.figge@stud.hs-bochum.de>
2024-05-09 15:57:46 +0200

    Revert "shortest path calculation stub"
    This reverts commit 8a2d738fa54765bef536e14f18bba51488c04ec7.

--- php/getPathPoints.php
+++ php/getPathPoints.php
@@ -10,16 +10,10 @@
     $dbCon = new NavDBConnection();
 
     // Get info on where to find navigation nodes in the db (tree id + node address)
-    $startNodeInfoArray = $dbCon->getNodeInfo($_POST['start']);
-    $finishNodeInfoArray = $dbCon->getNodeInfo($_POST['finish']);
+    $startNodeInfo = $dbCon->getNodeInfo($_POST['start']);
+    $finishNodeInfo = $dbCon->getNodeInfo($_POST['finish']);
 
-    if($startNodeInfoArray && $finishNodeInfoArray) {
-
-        $startNodeInfo = $startNodeInfoArray[0];
-        $finishNodeInfo = $finishNodeInfoArray[0];
-        if(count($startNodeInfoArray) > 1 || count($finishNodeInfoArray) > 1) {
-            // TODO get shortest path if any location is mapped to more than one node
-        }
+    if($startNodeInfo && $finishNodeInfo) {
 
         // "meta tree" path calculation if start & finish nodes are in different trees.
         // Returns an associative array with treeA_id => [startAdrA, finishAdrA], treeB_id => ... to iterate over
